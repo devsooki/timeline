9af1b957d9562d7447fdfdab56e367896745dda9
devsooki <devsooki@gmail.com>
2022-11-26 22:37:41 +0900

    feat: timeLine publishing

---
+++ src/pages/timeline/TimelineData.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TimelineData = () => {
+  const timeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+  return (
+    <>
+      {
+        timeArray.map(time => (
+          <Container>
+            <TimeContent>{time}시</TimeContent>
+            <Input />
+            <Button>등록</Button>
+          </Container>
+        ))
+      }
+    </>
+  )
+}
+
+export default TimelineData
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  color: #666;
+  border-bottom: 1px solid #ddd;
+`
+const TimeContent = styled.div`
+  width: 30px;
+  font-size: 10px;
+`
+const Input = styled.input`
+  flex: 1;
+  padding: 5px;
+  font-size: 13px;
+  border: none;
+`
+const Button = styled.button`
+  width: 30px;
+  height: 25px;
+  color: #c96d84;
+  font-size: 10px;
+  background-color: #fce4ea;
+`
--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,11 +1,13 @@
 import Header from 'components/Header';
 import React from 'react';
 import styled from 'styled-components';
+import TimelineData from './TimelineData';
 
 const TimeLine = () => {
   return (
     <Container>
       <Header />
+      <TimelineData />
     </Container>
   )
 }
@@ -15,6 +17,6 @@ export default TimeLine
 const Container = styled.div`
   margin: 30px auto;
   width: 400px;
-  height: calc(100vh - 60px);
+  //height: calc(100vh - 60px);
   background-color: rgba(255, 255, 255, 0.7);
 `
